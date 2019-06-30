36b7fdb625385ef4ef0a1944d56c7ad00c27d79f
sustained <sustained.dissonance@gmail.com>
2019-06-30 17:41:55 +0100

    Run ESLint/Prettier.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './registerServiceWorker';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
@@ -10,4 +10,4 @@ new Vue({
   router,
   store,
   render: h => h(App),
-}).$mount('#app');
+}).$mount("#app");

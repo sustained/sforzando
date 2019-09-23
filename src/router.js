cfb0d65779f75039a0ba680b7c1eef2393af4973
sustained <sustained.dissonance@gmail.com>
2019-09-23 22:12:43 +0100

    Update routes.

--- src/router.js
+++ src/router.js
@@ -14,12 +14,14 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/piano",
+      name: "piano",
+      component: () => import(/* webpackChunkName: "piano" */ "./views/Piano.vue")
+    },
+    {
+      path: "/circle",
+      name: "circle",
+      component: () => import(/* webpackChunkName: "circle" */ "./views/Circle.vue")
     }
   ]
 })

ea0198062aff88514dab63977e24f0154eeae704
sustained <sustained.dissonance@gmail.com>
2019-10-13 12:40:37 +0100

    Update library build.

--- src/libraryBuild.js
+++ src/libraryBuild.js
@@ -1,4 +1,3 @@
-import Piano from "./components/Piano.vue"
-import CircleOfFifths from "./components/CircleOfFifths.vue"
-
-export { Piano, CircleOfFifths }
+export { default as pianoState, reset } from "./library/piano-state"
+export { default as Piano } from "./components/Piano.vue"
+export { default as CircleOfFifths } from "./components/CircleOfFifths.vue"

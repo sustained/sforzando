8433f18eb9b480347428ec76bb417456bffda9b4
martijnmichel <38880192+martijnmichel@users.noreply.github.com>
2019-10-19 08:26:38 +0200

    fixed
    methods was defined twice & updated some css

--- src/components/Piano.vue
+++ src/components/Piano.vue
@@ -5,50 +5,77 @@
         v-for="(key, index) in keys"
         :key="index"
         :style="key.style"
-        @click="toggleActive(key.name)"
-        :class="[...key.class, {active: noteActive(key.name)}]"
+        :class="[...key.class, { active: noteActive(key.name) }]"
+        @mouseover="onMouseOver(key, $event)"
+        @click="onClick(key, $event)"
       >
-        <span>{{ key.name }}</span>
+        <span v-show="showNoteNames">{{ key.name }}</span>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import { clamp } from "@/library/math"
-import pianoState from "@/library/piano-state"
+import { clamp } from "./math";
+import pianoState from "./piano-state";
 
-const WHITE_KEYS = ["C", "D", "E", "F", "G", "A", "B"]
-const BLACK_KEYS = ["C#", "D#", null, "F#", "G#", "A#", null]
-const NOTE_OFFSETS = ["C", "D", "E", "F", "G", "A", "B"]
-const NUM_WHITE_KEYS_PER_OCTAVE = 7
-const NUM_BLACK_KEYS_PER_OCTAVE = 5
-const NUM_WHITE_KEYS_TOTAL = 52
-const NUM_BLACK_KEYS_TOTAL = 36
-const MIN_OCTAVE = 0
-const MAX_OCTAVE = 8
-const MIN_NOTE = 0
-const MAX_NOTE = 6
+const WHITE_KEYS = ["C", "D", "E", "F", "G", "A", "B"];
+const BLACK_KEYS = ["C#", "D#", null, "F#", "G#", "A#", null];
+//const NOTE_OFFSETS = ["C", "D", "E", "F", "G", "A", "B"];
+const NUM_WHITE_KEYS_PER_OCTAVE = 7;
+const NUM_BLACK_KEYS_PER_OCTAVE = 5;
+//const NUM_WHITE_KEYS_TOTAL = 52;
+//const NUM_BLACK_KEYS_TOTAL = 36;
+const MIN_OCTAVE = 0;
+const MAX_OCTAVE = 8;
+const MIN_NOTE = 0;
+const MAX_NOTE = 6;
 
 export default {
   props: {
+    emitEvents: {
+      type: Boolean,
+      default() {
+        return false;
+      }
+    },
+
+    display: {
+      default: undefined,
+      type: Object
+    },
+
+    showNoteNames: {
+      type: Boolean,
+      default() {
+        return false;
+      }
+    },
+
+    highlightPressed: {
+      type: Boolean,
+      default() {
+        return true;
+      }
+    },
+
     octaveStart: {
       type: Number,
       validator(value) {
-        return value >= MIN_OCTAVE && value <= MAX_OCTAVE
+        return value >= MIN_OCTAVE && value <= MAX_OCTAVE;
       },
       default() {
-        return MIN_OCTAVE
+        return MIN_OCTAVE;
       }
     },
 
     octaveEnd: {
       type: Number,
       validator(value) {
-        return value >= MIN_OCTAVE && value <= MAX_OCTAVE
+        return value >= MIN_OCTAVE && value <= MAX_OCTAVE;
       },
       default() {
-        return MAX_OCTAVE
+        return MAX_OCTAVE;
       }
     },
 
@@ -56,13 +83,13 @@ export default {
       type: [Number, String],
       validator(value) {
         if (typeof value === "string") {
-          return WHITE_KEYS.includes(value)
+          return WHITE_KEYS.includes(value);
         } else {
-          return value >= MIN_NOTE && value <= MAX_NOTE
+          return value >= MIN_NOTE && value <= MAX_NOTE;
         }
       },
       default() {
-        return WHITE_KEYS.indexOf("A")
+        return WHITE_KEYS.indexOf("A");
       }
     },
 
@@ -70,32 +97,32 @@ export default {
       type: [Number, String],
       validator(value) {
         if (typeof value === "string") {
-          return WHITE_KEYS.includes(value)
+          return WHITE_KEYS.includes(value);
         } else {
-          return value >= MIN_NOTE && value <= MAX_NOTE
+          return value >= MIN_NOTE && value <= MAX_NOTE;
         }
       },
       default() {
-        return WHITE_KEYS.indexOf("C")
+        return WHITE_KEYS.indexOf("C");
       }
     }
   },
 
   created() {
     if (typeof this.noteStart === "string") {
-      this.offsets.noteStart = WHITE_KEYS.indexOf(this.noteStart)
+      this.offsets.noteStart = WHITE_KEYS.indexOf(this.noteStart);
     } else {
-      this.offsets.noteStart = this.noteStart
+      this.offsets.noteStart = this.noteStart;
     }
 
     if (typeof this.noteEnd === "string") {
-      this.offsets.noteEnd = WHITE_KEYS.indexOf(this.noteEnd)
+      this.offsets.noteEnd = WHITE_KEYS.indexOf(this.noteEnd);
     } else {
-      this.offsets.noteEnd = this.noteEnd
+      this.offsets.noteEnd = this.noteEnd;
     }
 
-    this.offsets.octaveStart = this.octaveStart
-    this.offsets.octaveEnd = this.octaveEnd
+    this.offsets.octaveStart = this.octaveStart;
+    this.offsets.octaveEnd = this.octaveEnd;
 
     if (
       this.offsets.octaveStart > this.offsets.octaveEnd ||
@@ -104,13 +131,7 @@ export default {
     ) {
       throw new Error(
         "The start octave must be lower than or equal to the end octave and the start note must be lower than the end note."
-      )
-    }
-  },
-
-  methods: {
-    playNote(note) {
-      console.log(note)
+      );
     }
   },
 
@@ -122,29 +143,50 @@ export default {
         noteStart: 0,
         noteEnd: 0
       }
-    }
+    };
   },
 
   methods: {
+    playNote(note) {
+      console.log(note);
+    },
+    onClick(key) {
+      if (this.highlightPressed) {
+        this.toggleActive(key.name);
+      }
+
+      if (this.emitEvents) {
+        this.$emit("click", key.name);
+      }
+    },
+
+    onMouseOver(key) {
+      if (this.emitEvents) {
+        this.$emit("hover", key.name);
+      }
+    },
+
     noteActive(note) {
-      return pianoState[note] === true
+      return pianoState[note] === true;
     },
 
     toggleActive(note) {
-      pianoState[note] === true ? pianoState[note] = false : pianoState[note] = true
+      if (this.emitEvents) {
+        pianoState[note] = !pianoState[note];
+      }
     },
 
     calculateOctave(n) {
       return (
         Math.floor(n / NUM_WHITE_KEYS_PER_OCTAVE) +
         Math.max(MIN_OCTAVE, this.offsets.octaveStart)
-      )
+      );
     }
   },
 
   computed: {
     pianoState() {
-      return pianoState
+      return pianoState;
     },
 
     offsetStart() {
@@ -152,11 +194,11 @@ export default {
       //   return 5
       // }
 
-      return clamp(this.offsets.noteStart, MIN_NOTE, MAX_NOTE)
+      return clamp(this.offsets.noteStart, MIN_NOTE, MAX_NOTE);
     },
 
     offsetEnd() {
-      return clamp(this.offsets.noteEnd, MIN_NOTE, MAX_NOTE)
+      return clamp(this.offsets.noteEnd, MIN_NOTE, MAX_NOTE);
     },
 
     totalWhiteKeys() {
@@ -165,7 +207,7 @@ export default {
         this.numOctaves * NUM_WHITE_KEYS_PER_OCTAVE -
           this.offsetStart -
           (NUM_WHITE_KEYS_PER_OCTAVE - (this.offsetEnd + 1))
-      )
+      );
     },
 
     totalBlackKeys() {
@@ -174,11 +216,11 @@ export default {
         this.numOctaves * NUM_BLACK_KEYS_PER_OCTAVE -
           this.offsetStart -
           (NUM_BLACK_KEYS_PER_OCTAVE - (this.offsetEnd + 1))
-      )
+      );
     },
 
     totalKeys() {
-      return this.totalWhiteKeys + this.totalBlackKeys
+      return this.totalWhiteKeys + this.totalBlackKeys;
     },
 
     numOctaves() {
@@ -186,23 +228,23 @@ export default {
         1 +
         (Math.min(MAX_OCTAVE, this.offsets.octaveEnd) -
           Math.max(MIN_OCTAVE, this.offsets.octaveStart))
-      )
+      );
     },
 
     style() {
       return {
         "--keys": this.totalWhiteKeys,
         "--octaves": this.numOctaves
-      }
+      };
     },
 
     keys() {
-      const keys = []
+      const keys = [];
 
       // White keys.
       for (let i = this.offsetStart, j = 0; j < this.totalWhiteKeys; i++, j++) {
-        const octave = this.calculateOctave(i)
-        const keyName = WHITE_KEYS[i % 7]
+        const octave = this.calculateOctave(i);
+        const keyName = WHITE_KEYS[i % 7];
 
         // Skip < A0
         // if (octave === 0 && WHITE_KEYS.indexOf(keyName) < 5) {
@@ -215,27 +257,27 @@ export default {
           style: {
             "grid-column": `${j === 0 ? 1 : 4 + (j - 1) * 3} / span 3`
           }
-        }
+        };
 
-        keys.push(key)
+        keys.push(key);
       }
 
       // Black keys.
       for (let i = this.offsetStart, j = 0; j < this.totalWhiteKeys; i++, j++) {
-        const octave = this.calculateOctave(i)
-        const keyName = BLACK_KEYS[i % 7]
+        const octave = this.calculateOctave(i);
+        const keyName = BLACK_KEYS[i % 7];
 
         // Skip E♯ (F) or B♯ (C).
         if (!keyName) {
-          continue
+          continue;
         }
 
         // Skip > C8.
         if (octave >= 8) {
-          continue
+          continue;
         }
 
-        const keyNameClass = keyName.replace("#", "s")
+        const keyNameClass = keyName.replace("#", "s");
 
         const key = {
           name: `${keyName}${octave}`,
@@ -243,18 +285,18 @@ export default {
           style: {
             "grid-column": `${j === 0 ? 3 : 6 + (j - 1) * 3} / span 2`
           }
-        }
+        };
 
-        keys.push(key)
+        keys.push(key);
       }
 
-      return keys
+      return keys;
     }
   }
-}
+};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 body,
 ul {
   margin: 0;
@@ -262,9 +304,11 @@ ul {
 }
 
 .keyboard {
-  width: 100vw;
+  width: 100%;
   height: calc(260px - calc(var(--octaves) * 10px));
   overflow-x: hidden;
+  border-left: 2px solid $grey-5;
+  border-bottom: 2px solid $grey-4;
 }
 
 .keyboard ul {
@@ -277,14 +321,19 @@ ul {
 }
 
 li {
+  position: relative;
   text-align: center;
   background-color: white;
-  border: 1px solid black;
+
   display: flex;
   justify-content: center;
   align-items: flex-end;
   padding-bottom: 0.25rem;
   font-weight: bold;
+}
+
+li span {
+  pointer-events: none !important;
 }
 
 li.black span {
@@ -294,16 +343,68 @@ li.black span {
 }
 
 .white {
+  background: linear-gradient(to bottom, #fff 0%, #e9e9e9 100%);
   grid-row: 1 / span 3;
   z-index: 2;
+  border-right: 2px solid $grey-5;
+}
+
+.white.active {
+  border-bottom: 4px solid rgba(0, 0, 0, 0.65);
 }
 
 .black {
   grid-row: 1 / span 2;
-  background-color: black;
+  background: linear-gradient(45deg, #222 0%, #555 100%);
+  border-radius: 0 0 4px 4px;
+  /* border: 1px solid #000; */
+  /* box-shadow: -1px -1px 2px rgba(255, 255, 255, 0.2) inset,
+    0 -5px 2px 3px rgba(0, 0, 0, 0.3) inset, 0 2px 4px rgba(0, 0, 0, 0.4); */
   color: white;
   z-index: 3;
+  position: relative;
+  overflow: hidden;
 }
+
+.black::after {
+  content: " ";
+  position: absolute;
+  left: 0;
+  top: -20px;
+  right: 0;
+  bottom: 0;
+  box-shadow: inset 0px 0px 3px 5px rgba(255, 255, 255, 0.26);
+}
+
+.black:active {
+  background: linear-gradient(to right, #444 0%, #222 100%);
+}
+
+/*
+.white.active::before {
+    content: "";
+    width: 0px;
+    height: 0px;
+    border-width: 216px 5px 0px;
+    border-style: solid;
+    border-color: transparent transparent transparent rgba(0,0,0,0.1);
+    position: absolute;
+    left: 0px;
+    top: 0px;
+}
+
+.white.active::after {
+    content: "";
+    width: 0px;
+    height: 0px;
+    border-width: 216px 5px 0px;
+    border-style: solid;
+    border-color: transparent transparent transparent rgba(0,0,0,0.1);
+    position: absolute;
+    right: 0px;
+    top: 0px;
+}
+*/
 
 .blank {
   border-width: 0;
@@ -311,43 +412,163 @@ li.black span {
 }
 
 li {
-  transition: background-color 0.2s;
+  transition: all 0.2s ease-out;
 }
 
-.Fs.active {
-  background-color: rgb(174, 0, 0);
+.Fs.active::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgb(174, 0, 0) 200%
+  );
 }
-.G.active {
-  background-color: rgb(255, 0, 0);
+.G.active::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgb(255, 0, 0) 200%
+  );
 }
-.Gs.active {
-  background-color: rgb(255, 0, 0);
+.Gs.active::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgb(255, 0, 0) 200%
+  );
 }
-.A.active {
-  background-color: rgb(255, 102, 0);
+.A.active::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgb(255, 102, 0) 200%
+  );
 }
-.As.active {
-  background-color: rgb(255, 239, 0);
+.As.active::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgb(255, 239, 0) 200%
+  );
 }
-.B.active {
-  background-color: rgb(153, 255, 0);
+.B.active::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgb(153, 255, 0) 200%
+  );
 }
-.C.active {
-  background-color: rgb(0, 40, 255);
+.C.active::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgb(0, 40, 255) 200%
+  );
 }
-.Cs.active {
-  background-color: rgb(0, 255, 242);
+.Cs.active::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgb(0, 255, 242) 200%
+  );
 }
-.D.active {
-  background-color: rgb(0, 122, 255);
+.D.active::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgb(0, 122, 255) 200%
+  );
 }
-.Ds.active {
-  background-color: rgb(5, 0, 255);
+.Ds.active::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgb(5, 0, 255) 200%
+  );
 }
-.E.active {
-  background-color: rgb(71, 0, 237);
+.E.active::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgb(71, 0, 237) 200%
+  );
 }
-.F.active {
-  background-color: rgb(99, 0, 178);
+.F.active::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgb(99, 0, 178) 200%
+  );
 }
 </style>
